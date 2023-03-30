6bae8050b7f03e3b577c64deec0cc9ad12239d91
Hector McEwen <hector.mcewen@elev.ga.ntig.se>
2023-03-30 13:57:42 +0200

    old project

---
+++ Bosse2/Bosse2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+string mood;
+string sport;
+string song;
+
+Console.WriteLine("Goodmorning. How are you?");
+mood = Console.ReadLine();
+
+
+
+Console.WriteLine($"{mood} huh? thats nice. Question, what is your favorite sport?");
+sport = Console.ReadLine();
+
+Console.WriteLine($"{sport} is a dumb sport. Favorite song?");
+song = Console.ReadLine();
+
+Console.WriteLine($"{song} is an awful song. You have awful taste");
+Console.WriteLine("Fuck you");
+
+
+
+Console.ReadLine();
+
+
